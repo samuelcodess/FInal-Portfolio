ee296c449d1353bfd233c7945753119483a6b609
samuelcodess <sifenkwe@stu.cu.edu.ng>
2023-10-05 12:38:37 +0100

    changes made

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ var typed = new Typed(".typing",{
     Backspeed:50,
     loop:true
 })
+// script.js
+// Get all the navigation links
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Loop through the links and add a click event listener
+navLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        // Prevent the default link behavior (e.g., navigating to a new page)
+        event.preventDefault();
+        
+        // Remove the "active" class from all links
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+        
+        // Add the "active" class to the clicked link
+        this.classList.add('active');
+    });
+});
+
+
